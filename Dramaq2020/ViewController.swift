41400157297ae585cd2d4189ff956317691fd19a
PetrosTepoyan <petros@tepoyan.ru>
2020-03-05 20:05:17 +0400

    Initial Commit

---
+++ Dramaq2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dramaq2020
+//
+//  Created by Петрос Тепоян on 5/3/20.
+//  Copyright © 2020 Петрос Тепоян. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Dramaq2020Tests/Dramaq2020Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Dramaq2020Tests.swift
+//  Dramaq2020Tests
+//
+//  Created by Петрос Тепоян on 5/3/20.
+//  Copyright © 2020 Петрос Тепоян. All rights reserved.
+//
+
+import XCTest
+@testable import Dramaq2020
+
+class Dramaq2020Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
